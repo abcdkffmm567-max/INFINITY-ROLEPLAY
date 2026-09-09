bcbbe2f0fa7c8803feffad0bdfa11fc4a68027a5
abcdkffmm567-max <abcdkffmm567@gmail.com>
2026-09-09 23:47:23 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBNRNmZJyxQiAj_AjNHLf43knOEmQ9e-FU",
-  authDomain: "l2k-top-up-store.firebaseapp.com",
-  databaseURL: "https://l2k-top-up-store-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "l2k-top-up-store",
-  storageBucket: "l2k-top-up-store.firebasestorage.app"
+  apiKey: "AIzaSyDWxZPjXFtLPP4GfueoLdohMhGjvrD_z2c",
+  authDomain: "infinityroleplay.firebaseapp.com",
+  databaseURL: "https://infinityroleplay-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "infinityroleplay",
+  storageBucket: "infinityroleplay.firebasestorage.app",
+  messagingSenderId: "262561203529",
+  appId: "1:262561203529:web:36ebab6e99f9d1703c8698"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
-
 const storage = firebase.storage();
